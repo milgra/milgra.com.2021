511cd6a09809c24f7ed7df1aad5885fc33c8899c
Milan Toth <milgra@milgra.com>
2020-02-08 20:41:05 +0100

    vertical design under buttons

--- src/milgracom/core.cljs
+++ src/milgracom/core.cljs
@@ -11,47 +11,66 @@
 
 
 (def btnstyle {:color "#333333"
-               :background "none"
+               :background "#EEEEEEAA"
                :padding 0
                :margin "20px"
                :position "absolute"
                :left "0px"
                :top  "20px"
-               :font-size "2em"
+               :font-size "1.9em"
                :font-weight 400
                :font-family "-apple-system,BlinkMacSystemFont,Avenir,Avenir Next,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif"
                :border "none"})
 
 
+(def fltstyle {:background "#EEEEEEAA"
+               :padding 0
+               :margin "0px"
+               :position "absolute"
+               :width "200px"
+               :height "800px"
+               :left "0px"
+               :top  "0px"})
+
 (defn menubutton [ label labelch oldlabelch ]
   (let [oldindex (str/index-of oldlabelch label)
         index (str/index-of labelch label)
         oldpos (if (= label (@btn-state :oldactive))
-                 600
-                 (* oldindex 22))
+                 0
+                 (+ -500 (* oldindex 22)))
         pos (reagent/atom oldpos)
-        pos-spring (anim/spring pos {:mass 5.0 :stiffness 1.0 :damping 1.0})
+        pos-spring (anim/spring pos {:mass 5.0 :stiffness 0.5 :damping 3.0})
         newpos (if (= label (@btn-state :active))
-                 600
-                 (* index 22))]
+                 0
+                 (+ -500 (* index 22)))
+        size (reagent/atom (if (= label (@btn-state :active))
+                             600
+                             100))
+        size-spring (anim/spring size {:mass 5.0 :stiffness 0.5 :damping 3.0})
+        ]
     (fn a-button []
-      [:div {:style {:width "100%" :height "1em"}}
+      [:div
        [anim/timeout #(reset! pos newpos) 100]
-       [:input
-        {:type "button"
-         :key (str label "button")
-         :value label
-         :style (-> btnstyle
-                    (assoc :position "absolute")
-                    (assoc :transform (str "translate(" @pos-spring "px)")))
-         :on-click (fn [e]
-                     (let [newmenu (concat (filter #(not= % label) (@btn-state :labels)) [label])]
-                       (println label " : " newmenu)
-                       (swap! btn-state assoc :oldlabels (@btn-state :labels))
-                       (swap! btn-state assoc :labels newmenu)
-                       (swap! btn-state assoc :oldactive (@btn-state :active))
-                       (swap! btn-state assoc :active label)
-                       ))}]
+       [:div {
+              :style (-> fltstyle
+                         (assoc :background (str "#FF" index index "55" ))
+                         (assoc :transform (str "translate(" @pos-spring "px)"))
+                         (assoc :width @size-spring))
+              }
+        [:input
+         {:type "button"
+          :key (str label "button")
+          :value label
+          :style btnstyle
+;;                      (assoc :transform (str "translate(" @pos-spring "px)")))
+          :on-click (fn [e]
+                      (let [newmenu (concat (filter #(not= % label) (@btn-state :labels)) [label])]
+                        (println label " : " newmenu)
+                        (swap! btn-state assoc :oldlabels (@btn-state :labels))
+                        (swap! btn-state assoc :labels newmenu)
+                        (swap! btn-state assoc :oldactive (@btn-state :active))
+                        (swap! btn-state assoc :active label)
+                        ))}]]
        ])))
 
 (defn menu []
@@ -59,7 +78,15 @@
   (fn []
     (let [labelch (apply str (@btn-state :labels))
           oldlabelch (apply str (@btn-state :oldlabels))]
-      [:div
+      [:div {:style {:width "600px"
+                     :height "80px"
+                     :background "#EEEEEE"
+                     :position "absolute"
+                     :left 0
+                     :right 0
+                     :margin-left "auto"
+                     :margin-right "auto"
+                     }}
        (map (fn [label] [(menubutton label labelch oldlabelch)]) (@btn-state :labels))])))
 
 (defn submenu []
@@ -73,6 +100,7 @@
                    :width "600px"
                    :overflow-x "auto"
                    :white-space "nowrap"
+                   :background "#EFEFEF"
                    }}
      "February January 2019 December November October September August July June May April March February January 2018"])
   )
@@ -84,11 +112,11 @@
                     :right 0
                     :margin-left "auto"
                     :margin-right "auto"
-                    :top "200px"
+                    :top "130px"
                     :align "center"
                     :width "600px"
                     :height "600px"
-                    :background "#555555"}}
+                    :background "#EEEEEE"}}
      "February January 2019 December November October September August July June May April March February January 2018"])
     )
 
