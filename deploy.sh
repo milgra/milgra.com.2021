4551d0277d2544507425965050eacd14e16bce38
Milan Toth <milgra@milgra.com>
2020-03-07 00:57:44 +0100

    fixed shadow-cljs compile glitch with reanimated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-lein uberjar
+lein ring uberjar
 
 rsync -v -r -e ssh datomic-pro-0.9.6024 root@116.203.87.141:/root/
-rsync -v -r -e ssh milgra.com.server-0.1.0-SNAPSHOT-standalone.jar root@116.203.87.141:/root 
+rsync -v -r -e ssh milgra.com.server-0.1.0-SNAPSHOT-standalone.jar root@116.203.87.141:/root/
 
 nohup bin/transactor -Xmx256m -Xms256m dev.properties &
 nohup java -server -Xms256m -Xmx256m -Ddatomic.objectCacheMax=64m -Ddatomic.memoryIndexMax=64m -jar milgra.com.server-0.1.0-SNAPSHOT-standalone.jar &
