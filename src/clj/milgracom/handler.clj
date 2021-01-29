c04d6ec3524c4ad618e72ec67360bcf1825e2b58
Milan Toth <milgra@milgra.com>
2021-01-29 14:05:58 +0100

    fixed crash in case of non-existing database

--- src/clj/milgracom/handler.clj
+++ src/clj/milgracom/handler.clj
@@ -1,15 +1,15 @@
 (ns milgracom.handler
   (:require
-            [datomic.api :as d]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [clojure.data.json :as json]
-            [clojure.java.io :as io]
-            [milgracom.database :as db]
-            [crypto.password.scrypt :as password]
-            [ring.util.response :as resp]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]
+    [compojure.core :refer :all]
+    [compojure.route :as route]
+    [crypto.password.scrypt :as password]
+    [datomic.api :as d]
+    [milgracom.database :as db]
+    [ring.middleware.cors :refer [wrap-cors]]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [ring.util.response :as resp]))
 
 
 (defonce uri "datomic:dev://localhost:4334/milgracom")
@@ -19,9 +19,11 @@
 (defonce ip-to-result (atom []))
 (defonce number-names ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 (defonce types ["blog" "game" "app" "proto"])
-(defonce conn (d/connect uri))
+(defonce conn (atom nil))
 
-(defn get-client-ip [req]
+
+(defn get-client-ip
+  [req]
   (if-let [ips (get-in req [:headers "x-forwarded-for"])]
     (-> ips (clojure.string/split #",") first)
     (:remote-addr req)))
@@ -30,7 +32,7 @@
 (defn get-all-posts
   "get all posts"
   []
-  (let [db (d/db conn)
+  (let [db (d/db @conn)
         posts (d/q db/all-posts-all-data-q db)]
     posts))
 
@@ -38,7 +40,7 @@
 (defn get-all-comments
   "get all comments"
   []
-  (let [db (d/db conn)
+  (let [db (d/db @conn)
         posts (d/q db/all-comments-all-data-q db)]
     posts))
 
@@ -46,12 +48,13 @@
 (defn setup
   "create schema"
   []
-  (let [succ (d/create-database uri)] 
-    (if succ 
-      (let [db (d/db conn)]
-        (let [resp (d/transact conn db/post-schema)]
+  (let [succ (d/create-database uri)]
+    (reset! conn (d/connect uri))
+    (if succ
+      (let [db (d/db @conn)]
+        (let [resp (d/transact @conn db/post-schema)]
           (println "post schema insert resp" resp))
-        (let [resp (d/transact conn db/comment-schema)]
+        (let [resp (d/transact @conn db/comment-schema)]
           (println "comment schema insert resp" resp)))
       (println "db exists"))))
 
@@ -66,12 +69,12 @@
 (defn fillup
   "fill up db with dev data"
   []
-  (let [db (d/db conn)]
-    (let [resp (d/transact conn db/first-posts)]
+  (let [db (d/db @conn)]
+    (let [resp (d/transact @conn db/first-posts)]
       (println "post insert resp" resp))
     (let [postid ((first (first (get-all-posts))) :db/id)
           comment (assoc db/first-comment :comment/postid postid)
-          resp (d/transact conn [comment])]
+          resp (d/transact @conn [comment])]
       (println "comment insert resp" resp))))
 
 
@@ -79,11 +82,11 @@
   "get all months where posts exist"
   [type]
   (let [dbtype (keyword type)
-        db (d/db conn)
+        db (d/db @conn)
         dates (d/q db/all-post-months-by-type-q db dbtype)]
     (reverse (sort (set (map
-                         (fn [[{date :post/date}]] [(+ 1900 (.getYear date)) (inc (.getMonth date))])
-                         dates))))))
+                          (fn [[{date :post/date}]] [(+ 1900 (.getYear date)) (inc (.getMonth date))])
+                          dates))))))
 
 ;(get-post-months "blog")
 
@@ -91,9 +94,9 @@
   "get all tags in posts"
   [type]
   (let [dbtype (keyword type)
-        db (d/db conn)
+        db (d/db @conn)
         tags (d/q db/all-post-tags-by-type-q db dbtype)]
-    (reduce (fn [res item] (into res ((first item) :post/tags)) ) #{} tags)))
+    (reduce (fn [res item] (into res ((first item) :post/tags))) #{} tags)))
 
 ;(get-post-tags "blog")
 
@@ -103,14 +106,13 @@
   (let [dbyear (Integer/parseInt year)
         dbmonth (Integer/parseInt month)
         dbtype (keyword type)
-        db (d/db conn)
+        db (d/db @conn)
         endmonth (if (= dbmonth 12) 1 (+ dbmonth 1))
         endyear (if (= dbmonth 12) (+ dbyear 1) dbyear)
         start (clojure.instant/read-instant-date (format "%d-%02d-01T00:00:00" dbyear dbmonth))
         end (clojure.instant/read-instant-date  (format "%d-%02d-01T00:00:00" endyear endmonth))
         posts (d/q db/posts-between-dates-by-type-q db start end dbtype)]
-    (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts)))
-    ))
+    (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts)))))
 
 ;(get-posts-for-month "2020" "03" "blog")
 
@@ -118,10 +120,9 @@
   "get all posts for given year and month"
   [type]
   (let [dbtype (keyword type)
-        db (d/db conn)
+        db (d/db @conn)
         posts (d/q db/all-posts-all-data-by-type-q db dbtype)]
-    (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts)))
-    ))
+    (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts)))))
 
 
 ;(get-posts-for-type "game")
@@ -129,21 +130,19 @@
 (defn get-posts-for-tag
   "get post titles for given tag"
   [tag]
-  (let [db (d/db conn)
+  (let [db (d/db @conn)
         posts (d/q db/posts-for-tag-q db tag)]
-    (reverse (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts)))
-    )))
+    (reverse (sort-by :post/date (map #(assoc % :post/date (subs (pr-str (% :post/date)) 7 26)) (map first posts))))))
 
 ;(get-posts-for-tag "Music")
 
 (defn get-post
   [id]
   (let [dbid (Long/parseLong id)
-        db (d/db conn)
+        db (d/db @conn)
         entity (d/entity db dbid)
         post (into {} (seq entity))
-        result (assoc post :post/date (subs (pr-str (post :post/date)) 7 26))
-        ]
+        result (assoc post :post/date (subs (pr-str (post :post/date)) 7 26))]
     (list result)))
 
 ;(get-post "17592186045508")
@@ -152,11 +151,10 @@
   "returns comments for give post id"
   [postid]
   (let [dbpostid (Long/parseLong postid)
-        db (d/db conn)
+        db (d/db @conn)
         comments (d/q db/comments-for-post-q db dbpostid)]
 
-    (sort-by :comment/date (map #(assoc % :comment/date (subs (pr-str (% :comment/date)) 7 26)) (map first comments)))
-    ))
+    (sort-by :comment/date (map #(assoc % :comment/date (subs (pr-str (% :comment/date)) 7 26)) (map first comments)))))
 
 ;(get-post-comments "17592186045425")
 
@@ -169,10 +167,9 @@
                  :post/type dbtype
                  :post/tags tags
                  :post/date (clojure.instant/read-instant-date date)
-                 :post/content content
-                 }]
-          db (d/db conn)
-          resp (d/transact conn data)]
+                 :post/content content}]
+          db (d/db @conn)
+          resp (d/transact @conn data)]
       "OK")
     "Invalid pass or type"))
 
@@ -188,10 +185,9 @@
                  :post/type dbtype
                  :post/tags tags
                  :post/date (clojure.instant/read-instant-date date)
-                 :post/content content
-                 }]
-          db (d/db conn)
-          resp (d/transact conn data)]
+                 :post/content content}]
+          db (d/db @conn)
+          resp (d/transact @conn data)]
       "OK")
     "Invalid pass"))
 
@@ -201,7 +197,7 @@
   [pass id]
   (if (password/check pass epass)
     (let [dbid (Long/parseLong id)
-          resp (d/transact conn [[:db.fn/retractEntity dbid]])]
+          resp (d/transact @conn [[:db.fn/retractEntity dbid]])]
       "OK")
     "Invalid pass"))
 
@@ -220,7 +216,7 @@
                      :comment/content content
                      :comment/nick nick
                      :comment/date (new java.util.Date)}]
-              resp (d/transact conn data)]
+              resp (d/transact @conn data)]
           "OK")
         "Invalid code"))
     "Invalid parameters"))
@@ -231,7 +227,7 @@
   [pass id]
   (if (password/check pass epass)
     (let [dbid (Long/parseLong id)
-          resp (d/transact conn [[:db.fn/retractEntity dbid]])]
+          resp (d/transact @conn [[:db.fn/retractEntity dbid]])]
       "OK")
     "Invalid pass"))
 
@@ -255,7 +251,7 @@
 (defroutes app-routes
 
   ; resource related
-  
+
   (GET "/" [] (resp/redirect "/index.html"))
   (GET "/post/:id" [] (io/resource "public/index.html"))
   (GET "/projects" [] (io/resource "public/index.html"))
@@ -265,7 +261,7 @@
   (GET "/admin" [] (io/resource "public/index.html"))
 
   ; api related
-  
+
   (GET "/api-getmonths" [type] (json/write-str {:months (get-post-months type) :tags (get-post-tags type)}))
   (GET "/api-getpostsbydate" [year month type] (json/write-str {:posts (get-posts-for-month year month type)}))
   (GET "/api-getpostsbytag" [tag] (json/write-str {:posts (get-posts-for-tag tag)}))
@@ -276,23 +272,23 @@
   (GET "/api-addcomment" [postid nick text code :as request] (json/write-str {:result (add-comment postid nick text code request)}))
 
   ; admin related
-  
+
   (POST "/api-addpost" [pass title date tags type content] (json/write-str {:result (add-post pass title date tags type  content)}))
   (POST "/api-updatepost" [pass id title date tags type content] (json/write-str {:result (update-post pass id title date tags type content)}))
   (GET "/api-removepost" [pass id] (json/write-str {:result (remove-entity pass id)}))
   (GET "/api-removecomment" [pass id] (json/write-str {:result (remove-entity pass id)}))
-  
+
   (route/resources "/")
   (route/not-found "Not Found"))
 
+
 (def app
   (-> app-routes
-      (wrap-cors :access-control-allow-origin [#".*"]
+      (wrap-cors :access-control-allow-origin [#".*" #"http://localhost:8700"]
                  :access-control-allow-methods [:post :get]
                  :access-control-allow-credentials "true"
                  :Access-Control-Allow-Headers "Content-Type, Accept, Authorization, Authentication, If-Match, If-None-Match, If-Modified-Since, If-Unmodified-Since")
-      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
-      ))
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
 
 ; init database on start
 (setup)
